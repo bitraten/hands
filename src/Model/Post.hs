917a41cb556a9e28265f09321042a6d19ab442a8
bitraten <russell.hofmann@tum.de>
2014-07-13 03:07:31 +0200

    Refactor Model.Post

--- src/Model/Post.hs
+++ src/Model/Post.hs
@@ -10,21 +10,25 @@ import Network.URI (URI)
 import Database.Persist
 import Database.Persist.TH
 import Model.Fields
+import Model.User
 
 share [mkPersist sqlSettings, mkMigrate "migratePost"] [persistLowerCase|
 Post json
     slug Text
+    -- UserDomain
     domain Text
     body Text
+    -- Blaze.Html ?
     body_html Text
---    created_at UTCTime
---    updated_at UTCTime
---    tags [Text]
---    published_at UTCTime
+    -- Tag
+    tags [Text]
+    published_at UTCTime
+    -- Guid
     guid Text
---    edited_at UTCTime
+    edited_at UTCTime
     url URI
-    referenced_guid Text
+    -- Guid
+    referenced_guid Text Maybe
     UniqueGuid guid
     deriving Show
 |]
