15796cfc7a9e0cc9f62906e56a85e00fe2aee3b7
bitraten <rph@bitraten.de>
2014-07-16 13:49:18 +0200

    Enhance views

--- src/View/Application.hs
+++ src/View/Application.hs
@@ -1,15 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 module View.Application where
 
-import Prelude hiding (head, id)
+import Prelude hiding (head, id, div)
 import Model.User
 import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes hiding (title)
 
 html :: User -> Html -> Html -> Html
 html user pageTitle content = docTypeHtml $ do
-    head $ do 
+    head $ do
         title pageTitle
         link ! href "/assets/application.css" ! rel "stylesheet"
     body $ do
+        header ! class_ "with-flair" ! rolePrimary $ do
+            div ! class_ "header-container" $ do
+                a ! class_ "header-site-image" $ do
+                   img ! src ""
+                div ! class_ "header-site-info" $ do
+                    div ! class_ "header-site-name" $ do
+                        a ! href "/" $ toHtml $ userDisplay_name user
+                    div ! class_ "header-site-domain" $ do
+                        a ! href "/" $ toHtml $ userDomain user
+        navigation
         section ! id "main" $ content
+        footer ! rolePrimary $ do
+            p $ do
+                strong $ a ! href "/" $ toHtml $ userDisplay_name user
+                " is running on "
+                strong $ a ! href "https://github.com/bitraten/hands" $ "hands"
+                ", a clone of #pants"
+
+navigation :: Html
+navigation = do
+    nav ! rolePrimary $ ul $ do
+            li ! class_ "active" $ a ! href "/" $ "Blog"
+            li $ a ! href "/network" $ "Network"
+            li $ a ! href "/following" $ "Following"
+            li $ a ! href "/user/edit" $ "Settings"
+            li $ a ! href "/login" $ i ! class_ "fa fa-sign-out" $ ""
+
+rolePrimary = customAttribute "role" "primary"
--- src/View/Posts.hs
+++ src/View/Posts.hs
@@ -1,34 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
 module View.Posts where
 
-import Prelude hiding (show, div)
+import Prelude hiding (div)
 import Control.Monad (forM_)
+import Data.Time.Format (formatTime)
 import Model.Post
 import Model.User
+import System.Locale (defaultTimeLocale)
 import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes
+
 
 index :: User -> [Post] -> Html
 index user posts = do
     forM_ posts $ do
-        show user
+        View.Posts.show user
 
 show :: User -> Post -> Html
-show user post = do
+show author post = do
     article ! class_ "post" $ do
         header $ do
-            div ! class_ "post-avatar" $ do
-                a ! class_ "avatar" ! href "" $ do
-                    img ! alt "" ! src ""
             ul ! class_ "post-information" $ do
                 li ! class_ "post-author" $ do
-                    a ! href "" $ "author"
+                    div ! class_ "post-avatar" $
+                        a ! class_ "avatar" ! href uUrl $
+                            img ! alt "" ! src ""
+                    a ! href uUrl $ toHtml $ userDisplay_name author
                 li ! class_ "post-time" $ do
-                    a ! href "" $ "date"
-                    a ! href "" $ "time"
+                    a ! href "" $ date
+                    preEscapedToHtml middot
+                    a ! href pUrl $ time
         (div ! class_ "post-body") . preEscapedToHtml $ postBody_html post
         ul ! class_ "post-actions" $ do
             li ! class_ "post-edit-link" $ do
                 a ! href "" $ "edit"
             li ! class_ "post-reply-link" $ do
                 a ! href "" $ "reply"
+    where   uUrl = toValue . Prelude.show $ userUrl author
+            pUrl = toValue . Prelude.show $ postUrl post
+            date = toHtml $ formatTime defaultTimeLocale "%b %d" $ postPublished_at post
+            time = toHtml $ formatTime defaultTimeLocale "%H:%M" $ postPublished_at post
+            middot = " &middot; " :: String
